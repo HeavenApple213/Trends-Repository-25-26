52140aa590a02629e33bde3e2ad2f9fa7c6c5dfe
InTheKnow <mgeclipse19@gmail.com>
2026-05-15 00:56:15 +0800

    Add files via upload

---
+++ my_app_ZC22Am/SCRIPT.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.app_member(
+	id, name, description)
+	VALUES (2, 'Roni Magiting', 'Roni is also a BS IT student');
+
+INSERT INTO public.app_member(
+	id, name, description)
+	VALUES (3, 'Debu Maliksi', 'Roni is a BS CS student');
